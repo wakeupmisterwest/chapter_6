1c5acff7bca047380435b341c5d5541c72273909
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-24 13:31:23 -0400

    Add files via upload

---
+++ Starks_TIY_6_1.py
@@ -0,0 +1,12 @@
+person = {
+    'first name': 'julian',
+    'last name': 'starks',
+    'age': 16,
+    'city': 'fort wayne',
+    }
+
+
+print(f"His first name is {person['first name'].title()}.")
+print(f"His last name is {person['last name'].title()}.")
+print(f"He is {person['age']} years old.")
+print(f"He lives in {person['city'].title()}, Indiana.")
