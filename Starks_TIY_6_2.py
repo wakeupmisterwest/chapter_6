96a97b45d17d49af8fdce2ec2eb6968eb7e24841
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-24 13:57:48 -0400

    Add files via upload

---
+++ Starks_TIY_6_2.py
@@ -0,0 +1,23 @@
+favorite_numbers = {
+    'tilbert': 62,
+    'boberta': 723,
+    'car battery': 548494,
+    'zumbo sauce': 23,
+    'dave': 1,
+    }
+
+
+favorite_number = favorite_numbers['tilbert']
+print(f"Tilbert's favorite number is {favorite_number}.")
+
+favorite_number = favorite_numbers['boberta']
+print(f"Boberta's favorite number is {favorite_number}.")
+
+favorite_number = favorite_numbers['car battery']
+print(f"Car battery's favorite number is {favorite_number}.")
+
+favorite_number = favorite_numbers['zumbo sauce']
+print(f"Zumbo sauce's favorite number is {favorite_number}")
+
+favorite_number = favorite_numbers['dave']
+print(f"Dave's favorite number is {favorite_number}")
