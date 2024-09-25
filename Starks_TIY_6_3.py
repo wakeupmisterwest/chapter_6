ec7f3bb368883a99aa9a968645f7bb1db2ed5a70
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-25 12:00:46 -0400

    Add files via upload

---
+++ Starks_TIY_6_3.py
@@ -0,0 +1,14 @@
+glossary = {
+            'list': 'a collection of items in a particular order',
+            'string': 'a series of characters',
+            'float': 'any number with a decimal point',
+            'integer': 'a number',
+            'tuple': 'a list of items that cannot be changed',
+           }
+
+
+print(f"\nA list is {glossary['list']}.")
+print(f"\nA string is {glossary['string']}.")
+print(f"\nA float is {glossary['float']}.")
+print(f"\nA integer is {glossary['integer']}.")
+print(f"A tuple is {glossary[tuple]}.")
