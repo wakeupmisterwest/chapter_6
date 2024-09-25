62ee9e95a26437a133582db763f6f3a8a8059c4a
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-25 13:08:23 -0400

    Add files via upload

--- Starks_TIY_6_4.py
+++ Starks_TIY_6_4.py
@@ -4,13 +4,13 @@ glossary = {
     'float': 'any number with a decimal point',
     'integer': 'a number',
     'tuple': 'a list of items that cannot be changed',
-    'for loop':'a way of running lines of code repeatedly',
-    'if statement':'a conditional statement that executes specific lines of '
+    'for loop': 'a way of running lines of code repeatedly',
+    'if statement': 'a conditional statement that executes specific lines of '
                    'code whether a condition is true or false',
-    'slice':'a way of working of working with a specific group of items in a '
+    'slice': 'a way of working of working with a specific group of items in a '
             'list',
-    'function':'a block of code that performs a specific task',
-    'variable':'a storage location that holds a value or data',
+    'function': 'a block of code that performs a specific task',
+    'variable': 'a storage location that holds a value or data',
     }
 
 
