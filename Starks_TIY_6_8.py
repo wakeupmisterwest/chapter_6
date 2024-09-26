01808ddd0ccf1fdce9d1e05bff1e905b1e520a07
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-26 13:43:19 -0400

    Add files via upload

---
+++ Starks_TIY_6_8.py
@@ -0,0 +1,40 @@
+pets = []
+
+pet = {
+    'name': 'maxwell',
+    'animal': 'tuxedo cat',
+    'owner': 'dave',
+    }
+
+pets.append(pet)
+
+pet = {
+    'name': 'scoobert',
+    'animal': 'toy poodle',
+    'owner': 'bob',
+    }
+
+pets.append(pet)
+
+pet = {
+    'name': 'mr. fresh',
+    'animal': 'domestic shorthair',
+    'owner': 'stray',
+    }
+
+pets.append(pet)
+
+
+for pet in pets:
+    name = pet['name']
+    animal = pet['animal']
+    owner = pet['owner']
+
+    print(f"\nName: {name.title()}")
+    print(f"Animal: {animal.title()}")
+    if owner != 'stray':
+        print(f"Owner: {name.title()}.")
+    else:
+        print(f"Owner: {name.title()} is a stray and has no owner.")
+
+
